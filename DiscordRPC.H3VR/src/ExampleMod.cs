5a3ff50fe445446b9e7fa2e86fa6d14c249c29c8
Frityet <ambhogal01@gmail.com>
2021-04-18 15:54:08 -0700

    Setup

---
+++ DiscordRPC.H3VR/src/ExampleMod.cs
@@ -0,0 +1,12 @@
+﻿using Deli.Setup;
+
+namespace DiscordRPC.H3VR
+{
+    public class DiscordRPC : DeliBehaviour
+    {
+        public DiscordRPC()
+        {
+            Logger.LogInfo("Started DiscordRPC!");
+        }
+    }
+}
